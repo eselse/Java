bcd48e5104d59606ad4433acc21d17b8b8b2d1d3
eselse <bpytc23up@gmail.com>
2017-03-14 17:49:35 +0300

    update

---
+++ ShildtBeginnerGuide/Chapter 2/LogicalOpTable.java
@@ -0,0 +1,35 @@
+// Упражнение 2.2
+// Отображение таблицы истинности для логических операций
+class LogicalOpTable {
+	public static void main(String args[]) {
+		boolean p, q;
+
+		System.out.println("    ßТаблица истинности для логических операций");
+
+		System.out.println("-------------------------------------------------");
+		System.out.println("| P\t| Q\t| AND\t| OR\t| XOR\t| NOT\t|");
+		System.out.println("-------------------------------------------------");
+
+		p = true; q = true;
+		System.out.print("| " + p + "\t| " + q + "\t| ");
+		System.out.print((p & q) + "\t| " + (p | q) + "\t| ");
+		System.out.println((p ^ q) + "\t| " + (!p) + "\t|");
+
+		p = true; q = false;
+		System.out.print("| " + p + "\t| " + q + "\t| ");
+		System.out.print((p & q) + "\t| " + (p | q) + "\t| ");
+		System.out.println((p ^ q) + "\t| " + (!p) + "\t|");
+
+		p = false; q = true;
+		System.out.print("| " + p + "\t| " + q + "\t| ");
+		System.out.print((p & q) + "\t| " + (p | q) + "\t| ");
+		System.out.println((p ^ q) + "\t| " + (!p) + "\t|");
+
+		p = false; q = false;
+		System.out.print("| " + p + "\t| " + q + "\t| ");
+		System.out.print((p & q) + "\t| " + (p | q) + "\t| ");
+		System.out.println((p ^ q) + "\t| " + (!p) + "\t|");
+
+		System.out.println("-------------------------------------------------");
+	}
+}
