3356caadac965c3a8565e924876350555c5ecd15
eselse <bpytc23up@gmail.com>
2017-04-05 10:06:50 +0300

    update

--- ShildtBeginnerGuide/Chapter 6/Recursion.java
+++ ShildtBeginnerGuide/Chapter 6/Recursion.java
@@ -18,6 +18,15 @@ class Factiorial {
 		for(t = 1; t <= n; t++) result *= t;
 		return result;
  	}
+
+ 	// Рекурсивный метод для подсчета суммы всех чисел от 1 до d
+ 	int recSum(int d) {
+ 		int res;
+
+ 		if(d == 1) return 1;
+ 		res = recSum(d - 1) + d;
+ 		return res;
+ 	}
 }
 
 class Recursion {
@@ -28,6 +37,13 @@ class Recursion {
 		System.out.println("Факториал 3 равен " + f.factR(3));
 		System.out.println("Факториал 4 равен " + f.factR(4));
 		System.out.println("Факториал 5 равен " + f.factR(5));
+		System.out.println();
+
+		System.out.println("Вычисление суммы всех чисел вплоть " + 
+			"до полученного, рекурсивным способом");
+		System.out.println("Сумма всех чисел от 1 до 10 равна " + f.recSum(10));
+		System.out.println("Сумма всех чисел от 1 до 15 равна " + f.recSum(15));
+		System.out.println("Сумма всех чисел от 1 до 5 равна " + f.recSum(5));
 		System.out.println();
 
 		System.out.println("Вычисление итеративным способом");
