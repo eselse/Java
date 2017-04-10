f000d76c05e3c6ff1fc3816acc2454d021df20a0
eselse <bpytc23up@gmail.com>
2017-04-10 11:23:29 +0400

    update

--- ShildtBeginnerGuide/Chapter 9/ThrowDemo.java
+++ ShildtBeginnerGuide/Chapter 9/ThrowDemo.java
@@ -3,9 +3,9 @@ class ThrowDemo {
 	public static void main(String args[]) {
 		try {
 			System.out.println("До оператора throw");
-			throw new ArithmeticException();
+			throw new IndexOutOfBoundsException();
 		}
-		catch (ArithmeticException exc) {
+		catch (IndexOutOfBoundsException exc) {
 			// Перехватить исключение
 			System.out.println("Исключение перехвачено");
 		}
