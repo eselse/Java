2b316330d5e1f387f84829e28883d602b50761b5
eselse <bpytc23up@gmail.com>
2017-04-10 18:21:34 +0400

    update

--- ShildtBeginnerGuide/Chapter 9/MultiCatch.java
+++ ShildtBeginnerGuide/Chapter 9/MultiCatch.java
@@ -1,28 +1,27 @@
 // Использование средства группового перехвата исключений
 // Примечание: для компиляции этого кода требуется JDK 7
-// или более поздняя версия покмплекта разработчика
+// или более поздняя версия комплекта разработчика.
 class MultiCatch {
-	public static void main(String args[]) {
-		int a = 88;
-		int b = 0;
-		int result;
-		char chrs[] = { 'A', 'B', 'C' };
+    public static void main(String args[]) {
+        int a = 88, b = 0;
+        int result;
+        char chrs[] = {'A', 'B', 'C'};
 
-		for(int i = 0; i < 2; i++) {
-			try {
-				if(i == 0)
-					// Сгенерировать исключение ArithmeticException
-					result = a / b;
-				else 
-					// Сгенерировать исключение ArrayIndexOutOfBoundsException
-					chrs[5] = 'X';
-			}
-			// В этом операторе catch организуется перехват
-			// обоих исключений
-			catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
-				System.out.println("Перехваченное исключение: " + e);
-			}
-		}
-		System.out.println("После группового перехватчика исключений");
-	}
+        for(int i = 0; i < 2; i++) {
+            try {
+                if(i == 0)
+                    // Сгенерировать исключение ArithmeticException
+                    result = a / b;
+                else
+                    // Сгенерировать исключение ArrayIndexOutOfBoundsException
+                    chrs[5] = 'X';
+            }
+            // В этом операторе catch организуется перехват
+            // обоих исключений
+            catch(ArrayIndexOutOfBoundsException | ArithmeticException e) {
+                System.out.println("Перехваченное исключение: " + e);
+            }
+        }
+        System.out.println("После группового перехвата исключений");
+    }
 }
